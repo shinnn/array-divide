ade3902515c5e3387ea5e8aa966a9a3cf54633c8
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-05-26 02:32:37 +0900

    use ES.next code for testing

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var requireBowerFiles = require('require-bower-files');
-var test = require('tape');
+const requireBowerFiles = require('require-bower-files');
+const test = require('tape');
 
 function runTest(description, main) {
-  test(description, function(t) {
+  test(description, t => {
     t.plan(8);
 
     t.equal(main.name, 'arrayDivide', 'should have a function name.');
 
-    var fixture = ['a', 'b', 'c', 'd'];
+    const fixture = ['a', 'b', 'c', 'd'];
 
     t.deepEqual(
       main(['a', 'b', 'c', 'd'], 3),
@@ -36,26 +36,26 @@ function runTest(description, main) {
     );
 
     t.throws(
-      main.bind(null, arguments, 1),
+      () => main(arguments, 1),
       /TypeError.*must be an array/,
       'should throw a type error when the first argument is not an array.'
     );
 
     t.throws(
-      main.bind(null, [], '1'),
+      () => main([], '1'),
       /TypeError.*must be a number/,
       'should throw a type error when the second argument is not a number.'
     );
 
     t.throws(
-      main.bind(null, [], 0.999999),
+      () => main([], 0.999999),
       /RangeError.*must be 1 and more/,
       'should throw a range error when the second argument is less than 1.'
     );
   });
 }
 
-runTest('require(\'array-divide\')', require('./'));
+runTest('require(\'array-divide\')', require('.'));
 
 global.window = {};
 requireBowerFiles({self: true});
