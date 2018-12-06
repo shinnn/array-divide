2a9eec522ed637e44165e194cdb02a0e5eca4c1f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-12-07 04:16:48 +0900

    drop Bower support as it's no longer maintained

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # array-divide
 
-[![NPM version](https://img.shields.io/npm/v/array-divide.svg)](https://www.npmjs.com/package/array-divide)
-[![Bower version](https://img.shields.io/bower/v/array-divide.svg)](https://github.com/shinnn/array-divide/releases)
-[![Build Status](https://travis-ci.org/shinnn/array-divide.svg?branch=master)](https://travis-ci.org/shinnn/array-divide)
-[![Coverage Status](https://img.shields.io/coveralls/shinnn/array-divide.svg)](https://coveralls.io/r/shinnn/array-divide)
-[![devDependency Status](https://david-dm.org/shinnn/array-divide/dev-status.svg)](https://david-dm.org/shinnn/array-divide#info=devDependencies)
+[![npm version](https://img.shields.io/npm/v/array-divide.svg)](https://www.npmjs.com/package/array-divide)
+[![Build Status](https://travis-ci.com/shinnn/array-divide.svg?branch=master)](https://travis-ci.com/shinnn/array-divide)
+[![Coverage Status](https://img.shields.io/coveralls/shinnn/array-divide.svg)](https://coveralls.io/github/shinnn/array-divide)
 
-Divide an array into multiple arrays each of whom have the same number of items
+Divide an `Array` into multiple `Array`s each of whom have the same number of items
 
 ```javascript
 arrayDivide(['a', 'b', 'c', 'd'], 2); //=> [['a', 'b'], ['c', 'd']]
@@ -15,36 +13,22 @@ arrayDivide(['a', 'b', 'c', 'd'], 3); //=> [['a', 'b', 'c'], ['d']]
 
 ## Installation
 
-### Package managers
+[Use](https://docs.npmjs.com/cli/install) [npm](https://docs.npmjs.com/about-npm/).
 
-#### [npm](https://www.npmjs.com/)
-
-```sh
+```
 npm install array-divide
 ```
 
-#### [bower](http://bower.io/)
-
-```sh
-bower install array-divide
-```
-
-#### [Duo](http://duojs.org/)
+## API
 
 ```javascript
-const arrayDivide = require('shinnn/array-divide');
+import arrayDivide from 'array-divide';
 ```
-
-### Standalone
-
-[Download the script file directly.](https://raw.githubusercontent.com/shinnn/array-divide/master/browser.js)
-
-## API
 
 ### arrayDivide(*array*, *count*)
 
 *array*: `Array`  
-*count*: `Number` (the maximum number of items)  
+*count*: `number` (the maximum number of items)  
 Return: `Array` (a new array and doesn't break the original one)
 
 ```javascript
@@ -52,11 +36,11 @@ arrayDivide(['a', 'b', 'c', 'd'], 100); //=> [['a', 'b', 'c', 'd']]
 arrayDivide([], 100); //=> []
 
 arrayDivide(['a', 'b', 'c'], 0); // throw an error (count must be >= 1)
-arrayDivide(arguments, 1); // throw an error (doesn't support array-like object)
+arrayDivide(new Set(), 1); // throw an error (doesn't support array-like object)
 ```
 
 ## License
 
-Copyright (c) 2015 - 2016 [Shinnosuke Watanabe](https://github.com/shinnn)
+Copyright (c) 2015 - 2018 [Shinnosuke Watanabe](https://github.com/shinnn)
 
 Licensed under [the MIT License](./LICENSE).
--- test.js
+++ test.js
@@ -1,75 +1,64 @@
-'use strong';
+'use strict';
 
-const requireBowerFiles = require('require-bower-files');
+const arrayDivide = require('.');
 const test = require('tape');
 
-function runTest(description, main) {
-  test(description, t => {
-    t.plan(10);
+test('arrayDivide()', t => {
+  const fixture = ['a', 'b', 'c', 'd'];
 
-    t.equal(main.name, 'arrayDivide', 'should have a function name.');
+  t.deepEqual(
+    arrayDivide(['a', 'b', 'c', 'd'], 3),
+    [['a', 'b', 'c'], ['d']],
+    'should return divided arrays by count.'
+  );
 
-    const fixture = ['a', 'b', 'c', 'd'];
+  t.deepEqual(
+    fixture,
+    ['a', 'b', 'c', 'd'],
+    'should not break the original array..'
+  );
 
-    t.deepEqual(
-      main(['a', 'b', 'c', 'd'], 3),
-      [['a', 'b', 'c'], ['d']],
-      'should return divided arrays by count.'
-    );
+  t.deepEqual(
+    arrayDivide(['a'], 1),
+    [['a']],
+    'should return one array when the count is the same as the number of array items or more.'
+  );
 
-    t.deepEqual(
-      fixture,
-      ['a', 'b', 'c', 'd'],
-      'should not break the original array..'
-    );
+  t.deepEqual(
+    arrayDivide([], 9999999),
+    [],
+    'should return an empty array when the original is an empty array.'
+  );
 
-    t.deepEqual(
-      main(['a'], 1),
-      [['a']],
-      'should return one array when the count is the same as the number of array items or more.'
-    );
+  t.throws(
+    () => arrayDivide(new Set([0, 1, 2]), 1),
+    /^TypeError.*must be an array/,
+    'should throw a type error when the first argument is not an array.'
+  );
 
-    t.deepEqual(
-      main([], 9999999),
-      [],
-      'should return an empty array when the original is an empty array.'
-    );
+  t.throws(
+    () => arrayDivide([], '1'),
+    /^TypeError.*1 is not a number\. The second argument to array-divide must be a natural number\./,
+    'should throw a type error when the second argument is not a number.'
+  );
 
-    t.throws(
-      () => main(new Set([0, 1, 2]), 1),
-      /^TypeError.*must be an array/,
-      'should throw a type error when the first argument is not an array.'
-    );
+  t.throws(
+    () => arrayDivide([], 1.1),
+    /^Error.*1\.1 is not a natural number\. The second argument to array-divide must be a natural number\./,
+    'should throw a range error when the second argument is not a natural number.'
+  );
 
-    t.throws(
-      () => main([], '1'),
-      /^TypeError.*1 is not a number\. The second argument to array-divide must be a natural number\./,
-      'should throw a type error when the second argument is not a number.'
-    );
+  t.throws(
+    () => arrayDivide([], -1),
+    /^Error.*-1 is not a natural number\. The second argument to array-divide must be a natural number\./,
+    'should throw a range error when the second argument is less than 1.'
+  );
 
-    t.throws(
-      () => main([], 1.1),
-      /^Error.*1\.1 is not a natural number\. The second argument to array-divide must be a natural number\./,
-      'should throw a range error when the second argument is not a natural number.'
-    );
+  t.throws(
+    () => arrayDivide([], 0),
+    /^RangeError.*0 is less than 1\. The second argument to array-divide must be a natural number\./,
+    'should throw a range error when the second argument is 0.'
+  );
 
-    t.throws(
-      () => main([], -1),
-      /^Error.*-1 is not a natural number\. The second argument to array-divide must be a natural number\./,
-      'should throw a range error when the second argument is less than 1.'
-    );
-
-    t.throws(
-      () => main([], 0),
-      /^RangeError.*0 is less than 1\. The second argument to array-divide must be a natural number\./,
-      'should throw a range error when the second argument is 0.'
-    );
-  });
-}
-
-runTest('require(\'array-divide\')', require('.'));
-
-global.window = {};
-requireBowerFiles({self: true});
-
-runTest('window.arrayDivide', global.window.arrayDivide);
+  t.end();
+});
